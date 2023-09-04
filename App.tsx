990a54d2b80352dfbbc8bb476bb7bb95cc15a87e
Florian Grima <florian.grima@epitech.eu>
2023-09-04 17:02:43 +0200

    add eslint / prettierrc

--- App.tsx
+++ App.tsx
@@ -1,16 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/pages/LoginScreen';
 
 export default function App() {
   return LoginScreen();
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,13 +1,13 @@
-import { View, Text } from "react-native";
-import { Logo } from "../svg/Logo";
-import { LoginPageContainer } from "./LoginScreen.style";
+import React from 'react';
+import { Logo } from '../svg/Logo';
+import { LoginPageContainer } from './LoginScreen.style';
 
 function LoginScreen() {
-    return (
-        <LoginPageContainer>
-            <Logo/>
-        </LoginPageContainer>
-    )
+  return (
+    <LoginPageContainer>
+      <Logo />
+    </LoginPageContainer>
+  );
 }
 
 export default LoginScreen;
