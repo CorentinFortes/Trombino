fe33415ec2c8eeb83318d495af021003394e83f1
Florian Grima <florian.grima@epitech.eu>
2023-09-12 13:36:02 +0200

    update all buttons

--- src/components/Button/Button.style.tsx
+++ src/components/Button/Button.style.tsx
@@ -2,16 +2,14 @@ import { Button } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 export const ButtonContainer = styled(Button)<{ backgroundColor?: string }>`
-  background-color: ${({ backgroundColor }) => backgroundColor || '#1E1E1E'};
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
 `;
 
-export const ButtonText = styled.Text<{ color?: string }>`
+export const ButtonText = styled.Text`
   font-family: 'Rubik_Medium';
   font-size: 16px;
-  color: ${({ color }) => color || '#fff'};
   text-align: center;
 `;
